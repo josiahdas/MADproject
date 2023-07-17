009933f0481e8b92f9348c0ad1c9fe7e9d598466
josiahdas <107151821+josiahdas@users.noreply.github.com>
2023-07-16 22:44:34 -0400

    Add files via upload

---
+++ lib/backend/api_requests/api_calls.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+
+import '../../_add/extra/util.dart';
+
+import 'api_manager.dart';
+
+export 'api_manager.dart' show ApiCallResponse;
+
+const _kPrivateApiFunctionName = 'ffPrivateApiCall';
+
+class QouteCall {
+  static Future<ApiCallResponse> call() {
+    return ApiManager.instance.makeApiCall(
+      callName: 'qoute',
+      apiUrl: 'https://zenquotes.io/api/random',
+      callType: ApiCallType.GET,
+      headers: {},
+      params: {},
+      returnBody: true,
+      encodeBodyUtf8: false,
+      decodeUtf8: false,
+      cache: false,
+    );
+  }
+
+  static dynamic qoute(dynamic response) => getJsonField(
+        response,
+        r'''$[:].q''',
+      );
+}
+
+class ApiPagingParams {
+  int nextPageNumber = 0;
+  int numItems = 0;
+  dynamic lastResponse;
+
+  ApiPagingParams({
+    required this.nextPageNumber,
+    required this.numItems,
+    required this.lastResponse,
+  });
+
+  @override
+  String toString() =>
+      'PagingParams(nextPageNumber: $nextPageNumber, numItems: $numItems, lastResponse: $lastResponse,)';
+}
+
+String _serializeList(List? list) {
+  list ??= <String>[];
+  try {
+    return json.encode(list);
+  } catch (_) {
+    return '[]';
+  }
+}
+
+String _serializeJson(dynamic jsonVar, [bool isList = false]) {
+  jsonVar ??= (isList ? [] : {});
+  try {
+    return json.encode(jsonVar);
+  } catch (_) {
+    return isList ? '[]' : '{}';
+  }
+}
